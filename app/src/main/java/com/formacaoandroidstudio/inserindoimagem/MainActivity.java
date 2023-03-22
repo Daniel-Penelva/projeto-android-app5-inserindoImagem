373cb5a40c0d521a3820f48672555461bd27750f
Daniel Penelva <d4n.andrade@gmail.com>
2023-03-22 03:45:16 -0300

    Finalizando interface App de notícias

--- app/src/main/java/com/formacaoandroidstudio/inserindoimagem/MainActivity.java
+++ app/src/main/java/com/formacaoandroidstudio/inserindoimagem/MainActivity.java
@@ -24,5 +24,22 @@ public class MainActivity extends AppCompatActivity {
      *     opção 'show baseline' e automaticamente vai criar uma linha base onde você vai poder
      *     puxar essa linha base para outra linha base de um outro elemento.
      *
+     *
+     *  -> Adicionei o 'android:screenOrientation="sensor"' para poder rotacionar a tela.
+     *
+     *  -> Vamos criar um scrool para manipular a tela. Para isso vamos usar um componente
+     *     chamado ScrollView. Vamos manipular pelo código (perspectiva code) o activity_main.xml.
+     *     Vamos adicionar a tag ScrollView e mais algunmas configurações de xmlns (que no caso estão
+     *     na tag constraintLayout) e de altura e comprimento do layout.
+     *
+     *     Configurações dentro da tag scrollView:
+     *
+     *     xmlns:android="http://schemas.android.com/apk/res/android"
+     *     xmlns:app="http://schemas.android.com/apk/res-auto"
+     *     xmlns:tools="http://schemas.android.com/tools"
+     *     android:layout_height="match_parent"
+     *     android:layout_width="match_parent"
+     *     android:fillViewport="true"         -> para ocupar toda a tela quando o layout tem pouco conteúdo.
+     *
      * */
 }
