6c01e439b8fa6fdf4e94de1759960c8f200fdf14
Daniel Penelva <d4n.andrade@gmail.com>
2023-03-22 02:48:51 -0300

    Criando interface App de notícias

--- app/src/main/java/com/formacaoandroidstudio/inserindoimagem/MainActivity.java
+++ app/src/main/java/com/formacaoandroidstudio/inserindoimagem/MainActivity.java
@@ -16,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
      * Aba atrributes - para elemento imagem:
      *   -> srcCompat no Pick resources é possível alterar a imagem.
      *   -> scaleType define como a imagem vai ser definida no latout.
+     *      . centerCrop a imagem fica centralizada e corta o excesso da imagem.
+     *
+     *  -> Alinhamento show baseline - cria uma linha base para alinhar o elemento baseado num
+     *     outro elemento.
+     *     Para fazer isso, clica com o botão direito do mouse em cima do elemento, clica na
+     *     opção 'show baseline' e automaticamente vai criar uma linha base onde você vai poder
+     *     puxar essa linha base para outra linha base de um outro elemento.
+     *
      * */
 }
